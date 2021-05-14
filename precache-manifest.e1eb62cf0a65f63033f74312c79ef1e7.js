af999f5fbabd97736f42e03c1d969ecc064d090d
RoshRoslin <RoshRoslin@gmail.com>
2021-05-14 14:43:56 -0700

    Updates

---
+++ precache-manifest.e1eb62cf0a65f63033f74312c79ef1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3c75e787a9405e5cb0adff6c7cd17b",
+    "url": "/react-cart-project/index.html"
+  },
+  {
+    "revision": "6e3ba53b9dc0a8831e8f",
+    "url": "/react-cart-project/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/react-cart-project/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cart-project/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e3ba53b9dc0a8831e8f",
+    "url": "/react-cart-project/static/js/main.aa9949d1.chunk.js"
+  },
+  {
+    "revision": "2fb5b23cd34c0dd877b6",
+    "url": "/react-cart-project/static/js/runtime-main.5f098f9e.js"
+  }
+]);
